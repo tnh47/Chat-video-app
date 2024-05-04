ff61f71f09489e2d4aa2558ef5d524c80d2f5478
KhanhHuhu <kaakkaak222@gmail.com>
2024-05-04 11:47:10 +0700

    fix

--- Program.cs
+++ Program.cs
@@ -20,8 +20,7 @@ namespace Chat_video_app
             FirestoreHelper.SetEnvironmentVariable();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new LoginForm());
-            Application.Run(new Room2_host("khanh", "60000"));
+            Application.Run(new LoginForm());
         }
     }
 }
